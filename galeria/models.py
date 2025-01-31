d4d37a3b704e1e0914d446d197042a8380836555
silvio009 <rm550821@fiap.com>
2025-01-31 16:35:26 -0300

    Commit

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#Classe banco de dados models 
+#python manage.py makemigrations
+#python manage.py migrate
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length= 50, null= False, blank= False)   #Campo de caracteres
+    legenda = models.CharField(max_length=150, null= False, blank= False)  #Campo de caracteres
+    descricao = models.TextField(null=False, blank=False)     #Campo de texto
+    foto = models.CharField(max_length=50, blank= False, null= False) # nome da foto
+
+
+#nome do objeto no terminal
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,18 @@
 
 from django.shortcuts import render
 
+
+# Guarda a onde as requisições vão ser chamadas --> templetes/html
+
 def index (request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"nome" : "nebulosa de carina",
+        "legenda" : "webbtelecope.org / NASA / ames webb"},
+    2: {"nome" : "Galaxia NGC 1079",
+        "legenda" : "nasa.org / NASA / Huble" }
+}
+    
+    return render(request, 'galeria/index.html', {"cards" : dados}) # enviando os dados dos cards junto com o render 
 
 def imagem (request):
-    return render(request, 'galeria/imagem.html')
+    return render(request, 'galeria/imagem.html') 
